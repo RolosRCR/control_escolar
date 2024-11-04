b10730da8b8788bf17ffd4d7a8626ab589fc73e6
Roberto Carlos Ramirez <78807484+RolosRCR@users.noreply.github.com>
2024-11-04 06:31:57 -0600

    crud materias

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -13,6 +13,23 @@ class MateriaController extends Controller
         $materias = Materia::all();
         return view('materias.index', compact('materias'));
     }
+    public function create()
+    {
+        return view('materias.new');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_materia' => 'required|integer|digits:6|unique:materias,id_materia',
+            'nombre' => 'required|string|max:100',
+            'credito' => 'required|integer|min:2|max:8',
+        ]);
+
+        Materia::create($request->all());
+
+        return redirect()->route('materias.index')->with('success', 'Materia agregada exitosamente.');
+    }
 
     // Mostrar formulario de edición
     public function edit($id)
